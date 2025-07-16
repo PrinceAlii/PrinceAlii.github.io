96626d670f1cf8a3db7c3ec7b841bdc66c264ede
Ali B <30826345+PrinceAlii@users.noreply.github.com>
2025-07-16 21:58:41 +1000

    Fix base path for GH Pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig(({ mode }) => ({
-  base: './',
+  base: '/',
   root: '.',
   build: {
     outDir: 'dist',
