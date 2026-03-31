6b8454f25f14ce33d42981d704bb52956f728be0
Ali Bonagdaran <30826345+PrinceAlii@users.noreply.github.com>
2026-03-31 20:39:10 +1100

    initial: add spectre astro theme as base

---
+++ astro.config.ts
@@ -0,0 +1,42 @@
+import mdx from '@astrojs/mdx';
+import node from '@astrojs/node';
+import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config';
+import expressiveCode from 'astro-expressive-code';
+import spectre from './package/src';
+import { spectreDark } from './src/ec-theme';
+
+// https://astro.build/config
+const config = defineConfig({
+	site: 'https://ali.bonagdaran.com',
+	output: 'static',
+	integrations: [
+		expressiveCode({
+			themes: [spectreDark],
+		}),
+		mdx(),
+		sitemap(),
+		spectre({
+			name: 'Ali Bonagdaran',
+			themeColor: '#4169e1',
+			openGraph: {
+				home: {
+					title: 'Ali Bonagdaran',
+					description: 'Personal portfolio of Ali Bonagdaran.',
+				},
+				blog: {
+					title: 'Blog',
+					description: 'Thoughts and writings.',
+				},
+				projects: {
+					title: 'Projects',
+				},
+			},
+		}),
+	],
+	adapter: node({
+		mode: 'standalone',
+	}),
+});
+
+export default config;
